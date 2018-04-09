4476872df9df627e87ece8c55b7c694f7f6eb898
ChrisKao <chi1010860@gmail.com>
2018-04-09 14:53:47 +0800

    Vue.js Test

--- Learning.js
+++ Learning.js
@@ -1,3 +1,33 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+      family:[
+        {son:'Chris'},
+        {son:'Jason'},
+        {son:'Willy'}
+      ],
+      message: 'You loaded this page on ' + new Date().toLocaleString(),
+      isShow: true,
+    }
+})
+  
+var app2 = new Vue({
+    el:"#app-2",
+    data:{
+        todos:[
+        { text: 'a'},
+        { text: 'b'},
+        { text: 'c'},
+        ],
+        message:"What the fuck"
+    },
+    methods:{
+        reverseMessage: function(){
+        this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+  
 function createObj(key, value){
     const obj = {};
     obj[key] = value;
