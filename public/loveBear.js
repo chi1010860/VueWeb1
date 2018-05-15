e582eb749f0293d9f4793443c2a79f6c138e9295
ChrisKao <chi1010860@gmail.com>
2018-05-15 14:05:50 +0800

    正式版完成

--- public/loveBear.js
+++ public/loveBear.js
@@ -6,7 +6,7 @@ var app = new Vue({
     methods: {
         playSound: function () {
             var r = Math.ceil(Math.random() * 100);
-            if (!this.isBroken){
+            if (!this.isBroken) {
                 if (r <= 20) {
                     this.playMothercake();
                 }
@@ -18,16 +18,16 @@ var app = new Vue({
                 this.playMothercake();
             }
         },
-        playLove: function(){
+        playLove: function () {
             var love = new Audio("./sounds/love.mp3");
             love.play();
         },
-        playMothercake: function() {
+        playMothercake: function () {
             var mothercake = new Audio("./sounds/mothercake.mp3");
             mothercake.play();
             this.isBroken = true;
         },
-        playRepair: function() {
+        playRepair: function () {
             var repair = new Audio("./sounds/repair.mp3");
             repair.play();
             this.isBroken = false;
--- server.js
+++ server.js
@@ -1,15 +1,77 @@
-const http = require('http');
+const fs = require('fs');
+const express = require('express');
 
-const hostname = '127.0.0.1';
-//const hostname = '140.113.156.16'
-const port = 3000;
+const app = express(); // 產生Express Application 物件
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.listen(3000, function () {
+    console.log("Server has been built on http://localhost:3000/");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.use(express.static(__dirname + '/public'));
+
+app.use('/images', express.static(__dirname + '/images'));
+
+app.use('/sounds', express.static(__dirname + '/sounds'));
+
+app.get("/", function (req, res) {
+    res.send("<h1>Node.js Server Test</h1>");
 });
+
+app.get("/about", function (req, res) {
+    res.send("<h1>About page</h1>")
+});
+
+app.get("/about/about-me", function (req, res) {
+    res.send("<h1>About me page</h1>")
+});
+
+app.get("/blog", function (req, res) {
+    res.json({
+        text: 'wtf'
+    })
+});
+
+app.get("/post/:id", function (req, res) {
+    res.send(req.params.id)
+})
+
+app.get("/string/:name/:nickname", function (req, res) {
+    res.send(req.params.name + " is so " + req.params.nickname)
+})
+
+app.get("/api/users/:id", function (req, res) {
+    if (req.params.id == 1) {
+        res.json({
+            id: req.params.id,
+            name: "Joe",
+            age: 18
+        })
+    }
+    else if (req.params.id == 2) {
+        res.json({
+            id: req.params.id,
+            name: "John",
+            age: 22
+        })
+    }
+    else {
+        res.send("NOT FOUND")
+    }
+})
+
+app.get("/egg", function (req, res) {
+    res.send(req.query)
+})
+
+app.get("/fox", function (req, res) {
+    res.json(req.query)
+})
+
+app.get("/gogo/:go", function (req, res) {
+    res.send(req.params.go + JSON.stringify(req.query))
+})
+
+app.get("/api/query", function (req, res) {
+    var obj = req.query;
+    res.json(obj);
+})
